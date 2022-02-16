7c35a506c866895986ec1ffe327896869e04323a
Ricardo <rmorale5@jhu.edu>
2022-02-15 22:37:27 -0500

    started on the c_hexmain function

--- Assignment2/csf_assign02/c_hexmain.c
+++ Assignment2/csf_assign02/c_hexmain.c
@@ -4,4 +4,29 @@
 
 int main(void) {
   // TODO: implement the main function
+  char data_buf[16];
+  char offset_buf[10];
+  char converted_buf[2];
+  int char_read;
+  int offset = 16;
+  do {
+    hex_format_offset(offset, offset_buf);
+    offset_buf[8] = ':';
+    offset_buf[9] = ' ';
+    hex_write_string(offset_buf);
+    
+    char_read = hex_read(data_buf);
+
+    for (int i = 0; i < 16; i += 2) {
+      
+    }
+
+    
+    
+    hex_write_string(data_buf);
+    offset += 16;
+  } while (char_read > 0);
+  
+  
+  
 }
