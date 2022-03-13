8990941f57d6d485552d8dee517018a98b4adff4
Ricardo <rmorale5@jhu.edu>
2022-03-13 12:17:26 -0400

    worked some more on the cache simulation

--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -3,8 +3,41 @@
 #include <stdlib.h>
 #include <string>
 #include <sstream>
-
+#include <bits/stdc++.h>
 using namespace std;
+
+
+
+struct Slot {
+  unsigned tag;
+  unsigned index;
+  unsigned offset;
+  bool valid;
+  unsigned load_ts, access_ts;
+  bool dirty;
+};
+
+struct Set {
+  vector<Slot> slots;
+};
+
+struct Cache {
+  vector<Set> sets;
+  
+  unsigned num_sets;
+  unsigned block_size;
+  unsigned container_size;
+  
+  unsigned total_loads;
+  unsigned total_stores;
+  unsigned load_hits;
+  unsigned load_misses;
+  unsigned hits;
+  unsigned misses;
+  unsigned total_cycles;
+}
+
+
 
 int main(int argc, char* argv[]) {
 
@@ -64,18 +97,90 @@ int main(int argc, char* argv[]) {
     cerr << "error: neither fifo nor lru specified" << endl;
     return 1;
   }
-
+  
   //reading traces
   char load_store;
   string address;
   string third_var;
   string line;
-  
-  while (getline(cin,line)) {
-    //store each memory access field
-    stringstream ss;
-    ss << line;
-    ss >> load_store >> address >> third_var;
+
+  Cache mainCache;
+  mainCache.num_sets = num_sets;
+  mainCache.block_size = block_size;
+  mainCache.container_size = num_block_bytes;
+
+  for (int i = 0; i < mainCache.num_sets; i++) {
+    Set simple_set;
+    for (int j = 0; j < mainCache.block_size; j++) {
+      Slot simple_slot;
+      simple_set.push_back(simple_slot);
+    }
+    mainCache.sets.push_back(simiple_set);
+    
   }
+
+  unsigned time = 0;
+  if (mainCache.block_size == 1) {
+    unsigned indexes_used = 0;
+    while (getline(cin,line)) {
+      //store each memory access field
+      stringstream ss;
+      ss << line;
+      ss >> load_store >> address >> third_var;
+      
+      // hits happen when found data in cache
+      // miss when data not found in cache
+      // both for loading
+      // when miss reques from main memory, send data to cache, store in cache, send to CPU
+      if (load_store == 'l') {
+	// slots = 2^num_of_index_bits
+	// block size = 2^offset bits
+	// capacity = slots * block size
+	int num_index_bits = (log(mainCache.num_sets) / log(2)) / 4;
+	int num_tag_bits = (32 - num_index_bits) / 4;
+	string tag_str = address.substr(0, num_tag_bits);
+	string index_str = address.substr(num_tag_bits, num_index_bits);
+	int tag_size = tag_str.size();
+	int index_size = index_str.size();
+
+	usigned tag;
+	unsigned index;
+	
+	for (int i = 0; i < tag_size; i++) {
+	  if (tag_str[i] >= 'a') {
+	    tag += tag_str[i] - 'a' + 10;
+	  } else {
+	    tag += tag_str[i] - '0';
+	  }
+	  tag = tag << 4;
+	}
+	
+	for (int i = 0; i < index_size; i++) {
+	  if (index_str[i] >= 'a') {
+	    index += index_str[i] - 'a' + 10;
+	  } else {
+	    index += index_str[i] - '0';
+	  }
+	  index = index << 4;
+	}
+
+	for (int i = 0; i <indexes_used; i++) {
+	  
+	}
+	
+      }
+      else {
+	
+      }
+      
+    }
+    
+  } else if (mainCache.num_sets == 1) {
+
+  } else {
+
+  }  
+  
+  
   return 0;
 }
