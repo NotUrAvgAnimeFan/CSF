add3c7ac19cdc72bdfa67f3d506373170397930d
anakuri <ana.kuri02@gmail.com>
2022-03-12 20:55:01 -0500

    finished command line argument parsing and reading traces

--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -9,7 +10,7 @@ int main(int argc, char* argv[]) {
 
   //print an error if invalid number of parameters
   if (argc != 7) {
-    std::cerr << "error: invalid number of parameters";
+    cerr << "error: invalid number of parameters";
     return 1;
   }
   int num_sets = atoi(argv[1]);
@@ -20,25 +21,61 @@ int main(int argc, char* argv[]) {
   string eviction_type = argv[6];
 
 
-  //if cache parameters are invalid, print and error message and exit
+  //if any cache parameters are invalid, print and error message and exit
+  string set = argv[1];
+  string block = argv[2];
+  string block_bytes = argv[3];
+
+  if (set.find_first_not_of("0123456789") != string::npos || block.find_first_not_of("0123456789") != string::npos || block_bytes.find_first_not_of("0123456789") != string::npos) {
+    cerr << "error: invalid integer" << endl;
+    return 1;
+  }
+  
   if ((num_sets & (num_sets - 1)) != 0 || num_sets == 0) {
-    std::cerr << "error: number of sets isn't a power of 2" << endl;
+    cerr << "error: number of sets isn't a power of 2" << endl;
     return 1;
   }
   if (block_size == 0 || (block_size & (block_size - 1)) != 0) {
-    std::cerr << "error: block size isn't a power of 2" << endl;
+    cerr << "error: block size isn't a power of 2" << endl;
     return 1;
   }
-  if (block_size < 4) {
-    std::cerr << "error: block size is less than 4" <<endl;
+  if (num_block_bytes == 0 || (num_block_bytes & (num_block_bytes - 1)) != 0) {
+    cerr << "error: number of bytes in each block isn't a power of 2" << endl;
+    return 1;
+  }
+  if (num_block_bytes < 4) {
+    cerr << "error: block size is less than 4" <<endl;
     return 1;
   }
   if (write_hit.compare("write-back") == 0 && write_miss.compare("no-write-allocate") == 0) {
-    std::cerr << "error: write-back and no-write-allocate both specified" << endl;
+    cerr << "error: write-back and no-write-allocate both specified" << endl;
     return 1;
   }
 
-  //TODO: check that the integers are valid (ie not negative or doubles) and the strings are valid 
-  //atoi returns 0 if string, removes decimals for doubles, and keeps negative values negative
+  if (write_hit.compare("write-back") != 0 && write_hit.compare("write-through") != 0) {
+    cerr << "error: neither write-back nor write-through specified" << endl;
+    return 1;
+  }
+  if (write_miss.compare("write-allocate") != 0 && write_miss.compare("no-write-allocate") != 0) {
+    cerr << "error: neither write-allocate nor no-write-alllocate specified" << endl;
+    return 1;
+  }
+  if (eviction_type.compare("fifo") != 0 && eviction_type.compare("lru") != 0) {
+    cerr << "error: neither fifo nor lru specified" << endl;
+    return 1;
+  }
+
+  //reading traces
+  char load_store;
+  string address;
+  string third_var;
+  string line;
+  
+  while (getline(cin,line)) {
+    //store each memory access field
+    stringstream ss;
+    ss << line;
+    ss >> load_store >> address >> third_var;
+  }
   return 0;
 }
