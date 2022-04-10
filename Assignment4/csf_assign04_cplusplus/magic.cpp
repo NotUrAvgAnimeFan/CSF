267120bcbf4490bbaf68b3569cd7b6f01071f02a
Ricardo <rmorale5@jhu.edu>
2022-04-09 23:30:12 -0400

    working for header and section

--- Assignment4/csf_assign04_cplusplus/magic.cpp
+++ Assignment4/csf_assign04_cplusplus/magic.cpp
@@ -11,8 +11,84 @@
 #include <sys/unistd.h>
 #include <fcntl.h>
 
+#include <iostream>
+#include <string>
+
 #include "elf_names.h"
+using namespace std;
+
+void printHeaderStats(Elf64_Ehdr *elf_header) {
+  
+  const char* objectType = get_type_name(elf_header->e_type);
+  const char* machineType = get_machine_name(elf_header->e_machine);
+  
+  cout << "Object file type: " << objectType << endl;
+  cout << "Instruction set: " << machineType << endl;
+  
+  if ((int)elf_header->e_ident[EI_DATA] == 1) {
+    cout << "Endianness: Little endian" << endl;
+  } else {
+    cout << "Endianness: Big endian" << endl;
+  }
+  
+}
+
 
 int main(int argc, char **argv) {
   // TODO: implement
+  
+  int fd = open(argv[1], O_RDONLY);
+
+  if (fd < 0) {
+    cerr << "could not open file";
+    return -1;
+  }
+
+  struct stat statbuf;
+  size_t file_size;
+  int rc = fstat(fd, &statbuf);
+  if (rc != 0) {
+
+    //error
+    cerr << "could not get info on file";
+    return -2;
+  } else {
+    file_size = statbuf.st_size;
+  }
+
+  void *data = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
+
+  if (data == ((void *)-1)) {
+    cerr << "could not get contents of file";
+    return -3;
+  }
+
+  Elf64_Ehdr *elf_header = (Elf64_Ehdr *) data;
+
+  if ((int)elf_header->e_ident[EI_MAG1] != 69 || (int)elf_header->e_ident[EI_MAG2] != 76 || (int)elf_header->e_ident[EI_MAG3] != 70) {
+    printf("Not an ELF file\n");
+    return 0;
+  }
+
+  printHeaderStats(elf_header);
+
+  Elf64_Shdr *section_header = (Elf64_Shdr*)((char*)elf_header + elf_header->e_shoff);
+
+  
+  Elf64_Shdr *section_with_names = &section_header[elf_header->e_shstrndx];
+  //data + section_wit_names->sh_offset + sec_header->sh_name;
+  uint8_t counter = 0;
+  Elf64_Shdr *start_names = (Elf64_Shdr*)(section_with_names->sh_offset + (char*)elf_header);  
+  //char nameOfSection[4] = {'y', 'e', 's', '\0'};
+  
+  while (counter < elf_header->e_shnum) {
+    
+    printf("Section header %d: name=%s, type=%lx, offset=%lx, size=%lx\n", counter, (char*)start_names + section_header[counter].sh_name , section_header[counter].sh_type, section_header[counter].sh_offset, section_header[counter].sh_size);
+    counter += 1;
+
+  }
+
+  
+  
+  
 }
