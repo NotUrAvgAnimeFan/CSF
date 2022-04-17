070b44c20167b5943c81320a2b32433d743ec034
Ricardo <rmorale5@jhu.edu>
2022-04-17 09:19:21 -0400

    created repository for HW5

---
+++ Assignment5/csf_assign05/connection.cpp
@@ -0,0 +1,52 @@
+#include <sstream>
+#include <cctype>
+#include <cassert>
+#include "csapp.h"
+#include "message.h"
+#include "connection.h"
+
+Connection::Connection()
+  : m_fd(-1)
+  , m_last_result(SUCCESS) {
+}
+
+Connection::Connection(int fd)
+  : m_fd(fd)
+  , m_last_result(SUCCESS) {
+  // TODO: call rio_readinitb to initialize the rio_t object
+  rio_readinitb(&m_fdbuf, fd);
+}
+
+void Connection::connect(const std::string &hostname, int port) {
+  // TODO: call open_clientfd to connect to the server
+  m_fd(open_clientf(hostname, &port));
+  // TODO: call rio_readinitb to initialize the rio_t object
+  rio_readinitb(&m_fdbuf, m_fd)
+
+  
+}
+
+Connection::~Connection() {
+  // TODO: close the socket if it is open
+  close(m_fd);
+}
+
+bool Connection::is_open() const {
+  // TODO: return true if the connection is open
+}
+
+void Connection::close() {
+  // TODO: close the connection if it is open
+}
+
+bool Connection::send(const Message &msg) {
+  // TODO: send a message
+  // return true if successful, false if not
+  // make sure that m_last_result is set appropriately
+}
+
+bool Connection::receive(Message &msg) {
+  // TODO: send a message, storing its tag and data in msg
+  // return true if successful, false if not
+  // make sure that m_last_result is set appropriately
+}
