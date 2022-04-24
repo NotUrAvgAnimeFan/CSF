4ceed6059cef2d11b122931f837a227b7ff4fd46
Ricardo <rmorale5@jhu.edu>
2022-04-23 20:37:05 -0400

    fixing bugs and making sure all errors caught correctly

--- Assignment5/csf_assign05/sender.cpp
+++ Assignment5/csf_assign05/sender.cpp
@@ -7,6 +7,74 @@
 #include "connection.h"
 #include "client_util.h"
 
+
+void userInput(Connection &conn) {
+  std::string input;
+  std::string command;
+  std::string roomToJoin;
+  Message toSend;
+  Message received;
+
+  std::cout << "> ";
+  
+  while(getline(std::cin, input)) {
+        
+    if (input[0] == '/') {
+      if (input.substr(1,4) == "join") {
+	std::stringstream s(input);
+	std::getline(s, command, ' ');
+	std::getline(s, roomToJoin);
+
+	
+	if (roomToJoin.size() > 0) {
+	  toSend = Message("join", input.substr(6));
+	} else {
+	  std::cerr << "/join command needs room name" << std::endl;
+	  std::cout << "> ";
+	  continue;
+	}
+	
+	
+      } else if (input == "/leave") {
+	toSend = Message("leave", "");
+      } else if (input == "/quit") {
+	toSend = Message("quit", "");
+      } else {
+	std::cerr << "command not defined" << std::endl;
+	continue;
+      }
+      
+    } else {
+      toSend = Message("sendall", input.substr(0, input.size()));
+    }
+
+    
+    if (conn.send(toSend)) {
+      
+      conn.receive(received);
+
+      if (toSend.tag == "quit") {
+	break;
+      }
+      
+      if (received.tag == "err") {
+	std::cerr << received.data << std::endl;
+      }
+      
+      
+    } else if (conn.get_last_result() == conn.INVALID_MSG) {
+      std::cerr << "invalid tag used or total message sized exceeded 250 characters" << std::endl;
+      
+    } else {
+      std::cerr << "message send was unsuccessful" << std::endl;
+    }
+
+    std::cout << "> ";
+  }
+  
+}
+
+
 int main(int argc, char **argv) {
   if (argc != 4) {
     std::cerr << "Usage: ./sender [server_address] [port] [username]\n";
@@ -63,19 +131,36 @@ int main(int argc, char **argv) {
   
   // TODO: loop reading commands from user, sending messages to
   //       server as appropriate
+
+  userInput(conn);
   
+  /*
   std::string input;
+  std::string command;
+  std::string roomToJoin;
   Message toSend;
   Message received;
 
   std::cout << "> ";
   
   while(getline(std::cin, input)) {
-    
-    
+        
     if (input[0] == '/') {
       if (input.substr(1,4) == "join") {
-	toSend = Message("join", input.substr(6));
+	std::stringstream s(input);
+	std::getline(s, command, ' ');
+	std::getline(s, roomToJoin);
+
+	
+	if (roomToJoin.size() > 0) {
+	  toSend = Message("join", input.substr(6));
+	} else {
+	  std::cerr << "/join command needs room name" << std::endl;
+	  std::cout << "> ";
+	  continue;
+	}
+	
+	
       } else if (input == "/leave") {
 	toSend = Message("leave", "");
       } else if (input == "/quit") {
@@ -112,7 +197,7 @@ int main(int argc, char **argv) {
 
     std::cout << "> ";
   }
-
+  */
 
   
   return 0;
